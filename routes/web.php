d2b58e8e695222269aeb60f0029ca131b8c7fa63
EduardoGMora <lalogomo2000@outlook.com>
2024-07-25 11:50:03 -0600

    feature: se agregó la función de busqueda en el header

--- routes/web.php
+++ routes/web.php
@@ -46,6 +46,7 @@ Route::get('/post/{post}',[PostController::class, 'showSinglePost']);
 Route::delete('/post/{post}',[PostController::class, 'deletePost'])->middleware('can:delete,post');
 Route::get('/post/{post}/edit',[PostController::class, 'showEditForm'])->middleware('can:update,post');
 Route::put('/post/{post}',[PostController::class, 'updatePost'])->middleware('can:update,post');
+Route::get('/search/{term}',[PostController::class, 'search']);
 
 //profile related routes
 Route::get('/profile/{user:username}',[UserController::class, 'showProfile']);
